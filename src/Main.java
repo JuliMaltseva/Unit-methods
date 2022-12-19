32c7747d76f65910dc66f5bf1a877495d6b3d5cf
Юлия <maltssevaulia@gmail.com>
2022-12-19 23:43:22 +0300

    all

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,36 @@
 public class Main {
+
+    public static void printSeparator() {
+        System.out.println("+++++++++++++++++");
+        System.out.println("_________________");
+    }
+
+    public static void printIssues(int issueCount) {
+        System.out.println(issueCount);
+    }
+
+
+    public static int sum(int[] numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
-
-
+// отработка заданий из видеоурока
+        int[] issuesByMonths = {4, 6, 7, 9, 2, 5, 12, 3, 10, 6, 7, 1, 8};
+        printSeparator();
+        for (int i = 0; i < issuesByMonths.length; i++) {
+            printIssues(issuesByMonths[i]);
+            if ((i + 1) % 3 == 0) {
+                printSeparator();
+            }
+        }
+        printSeparator();
+        int total = sum(issuesByMonths);
+        printIssues(total);
 
 
     }
